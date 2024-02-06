216c44036b95034c3553712a5df116db8e1c2f54
Tong Zhang <zhangt@frib.msu.edu>
2024-02-06 16:00:58 -0500

    REF: Update the libcadata.dll.

--- examples/example_glfw_opengl3/run-win32.bat
+++ examples/example_glfw_opengl3/run-win32.bat
@@ -1,6 +1,10 @@
 @ECHO OFF
-SET EPICS_CA_AUTO_ADDR_LIST=YES
+:: SET "EPICS_CA_AUTO_ADDR_LIST=YES"
+SET "EPICS_CA_AUTO_ADDR_LIST=NO"
+SET "EPICS_CA_NAME_SERVERS=127.0.0.1:50640"
+SET "EPICS_CA_MAX_ARRAY_BYTES=32000000"
 
 copy example_glfw_opengl3.exe dist\win32
+copy cadata\libcadata.dll dist\win32
 cd dist\win32\
 example_glfw_opengl3
